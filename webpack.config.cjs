b8c9e3c5588f063dc51fd30b09d2c8c7e4548026
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2023-04-16 11:47:52 +0700

    fix config

---
+++ webpack.config.cjs
@@ -0,0 +1,122 @@
+//@ts-check
+const { EsbuildPlugin } = require('esbuild-loader')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+require('dotenv').config()
+
+/**
+ * @type {(dev: boolean, name: string) => import('webpack').Configuration}
+ */
+const config = (dev, name) => ({
+    mode: dev ? 'development' : 'production',
+    entry: './src/main.tsx',
+    output: {
+        filename: `static/${name}.js`,
+        path: path.resolve(__dirname, 'public'),
+        clean: true,
+    },
+    target: ['web', 'es6'],
+    optimization: {
+        minimize: !dev,
+        minimizer: [
+            new EsbuildPlugin({
+                target: 'es6',
+                css: true,
+            }),
+        ],
+        runtimeChunk: 'single',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.tsx$/i,
+                loader: 'esbuild-loader',
+                options: {
+                    loader: 'tsx',
+                    target: 'es6',
+                },
+            },
+            {
+                test: /\.ts$/i,
+                loader: 'esbuild-loader',
+                options: {
+                    loader: 'ts',
+                    target: 'es6',
+                },
+            },
+            {
+                test: /\.(png|jpe?g|webp)/,
+                loader: 'file-loader',
+                options: {
+                    name: `static/${name}.[ext]`,
+                },
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/inline',
+            },
+            {
+                test: /\.md$/,
+                type: 'asset/source',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            publicPath: '/',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true,
+                minifyCSS: true,
+                minifyJS: true,
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: `static/${name}.css`,
+        }),
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    //@ts-ignore
+    devServer: {
+        host: '0.0.0.0',
+        port: process.env.WEBPACK_DEV_SERVER_PORT || 8081,
+        static: './public',
+        hot: true,
+        historyApiFallback: true,
+        //@ts-ignore
+        proxy: {
+            '*': 'http://localhost:' + process.env.PORT || 8080,
+        },
+        devMiddleware: {
+            writeToDisk: true,
+        },
+    },
+    devtool: dev ? 'source-map' : false,
+    performance: {
+        maxAssetSize: 512000,
+    },
+})
+
+module.exports = function (env, argv) {
+    console.log(argv)
+    const dev = argv.mode == 'development'
+    const name = dev ? '[name]' : '[contenthash]'
+
+    return config(dev, name)
+}
